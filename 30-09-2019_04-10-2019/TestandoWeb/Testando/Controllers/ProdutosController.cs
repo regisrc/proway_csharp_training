47f6bcbeda4077b1eed9932dd2f9d10868a279a9
franz <tmppmrv2@outlook.com>
2019-10-02 08:36:38 -0300

    Commit

---
+++ 30-09-2019_04-10-2019/TestandoWeb/Testando/Controllers/ProdutosController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Testando.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
---
+++ 30-09-2019_04-10-2019/TestandoWeb/Testando/Models/ProdutoViewMode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testando.Models
+{
+    public class ProdutoViewMode
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+        public double QtdEstoque { get; set; }
+        public bool Ativo { get; set; }
+
+    }
+}
